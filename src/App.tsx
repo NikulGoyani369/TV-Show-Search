7e63a0cc5e394b98b8660949d46762de45eb52c0
Nikul Goyani <goyaniniks@gmail.com>
2024-03-26 22:12:36 +0000

    WIP searchPeople promise withn return pople array object

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,38 @@ interface FavoriteShow {
   id: number;
   name: string;
 }
+// {"id":189609,"url":"https://www.tvmaze.com/people/189609/cigarettes-after-sex",
+// "name":"Cigarettes After Sex","country":null,"birthday":null,"deathday":null,"gender":null,"image":null,"updated":1512730582,
+// "_links":{"self":{"href":"https://api.tvmaze.com/people/189609"}}}
 
+// People interface
+interface People {
+  id: number;
+  name: string;
+  country: string;
+  birthday: string;
+  deathday: string;
+  gender: string;
+  image: {
+    medium: string;
+    original: string;
+  };
+  updated: number;
+  _links: {
+    self: {
+      href: string;
+    };
+  };
+}
 
 // App function
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [shows, setShows] = useState<Show[]>([]);
+  const [people, setPeople] = useState<People[]>([]);
   const [favorites, setFavorites] = useState<FavoriteShow[]>([]);
   const [selectedShow, setSelectedShow] = useState<Show | null>(null);
+  const [selectedPeople, setSelectedPeople] = useState<People | null>(null);
   const [showModal, setShowModal] = useState(false);
 
   // Load favorites from local storage on initial render
@@ -54,11 +78,6 @@ function App() {
       const searchResponse = await fetch(`https://api.tvmaze.com/api/search/shows?q=${searchTerm}`);
       const searchJson: any[] = await searchResponse.json();
 
-      const searchPeopeResponse = await fetch(`https://api.tvmaze.com/api/search/people?q=${searchTerm}`);
-      const searchPeopleJson: any[] = await searchPeopeResponse.json();
-
-      console.log(searchPeopleJson);
-
       console.log(searchJson);
 
       if (searchJson && searchJson.length > 0) {
@@ -70,25 +89,40 @@ function App() {
           summary: stripHtmlTags(item.show.summary),
           image: item.show.image,
         }));
-        setShows(processedShows);
-      }
-
-      // WIP for people search
-      if (searchPeopleJson && searchPeopleJson.length > 0) {
-        const processedShows: Show[] = searchPeopleJson.map((item) => ({
-          id: item.person.id,
-          name: item.person.name,
-          rating: item.person.rating ? item.person.rating : 'N/A',
-          genres: item.person.genres,
-          summary: stripHtmlTags(item.person.summary),
-          image: item.person.image,
-        }));
-        setShows(processedShows);
+       
+        return setShows(processedShows);
       }
     } catch (error) {
       console.log(error);
     }
   };
+
+  const searchPeople = async () => {
+    try {
+      const searchResponse = await fetch(`https://api.tvmaze.com/api/search/people?q=${searchTerm}`);
+      const searchPersonJson: any[] = await searchResponse.json();
+
+      console.log(searchPersonJson);
+
+      if (searchPersonJson) {
+        const processedShows: People[] = searchPersonJson.map((item) => ({
+          id: item.person.id,
+          name: item.person.name,
+          country: item.person.country ? item.person.country : 'N/A',
+          birthday: item.person.birthday ? item.person.birthday : 'N/A',
+          deathday: item.person.deathday ? item.person.deathday : 'N/A',
+          gender: item.person.gender ? item.person.gender: 'N/A',
+          image: item.person.image,
+          updated: item.person.updated,
+          _links: item.person._links,
+        }));
+        
+        return setPeople(processedShows);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const addFavorite = (show: Show) => {
     const newFavorite: FavoriteShow = {
@@ -119,6 +153,12 @@ function App() {
     setShowModal(true);
   };
 
+  // function to open modal
+  const openModel2 = (person: People) => {
+    setSelectedPeople(person);
+    setShowModal(true);
+  };
+
   // function to close modal
   const closeModel = () => {
     setShowModal(false);
@@ -138,7 +178,7 @@ function App() {
             onChange={(e) => setSearchTerm(e.target.value)}
             className="input-field"
           />
-          <button className="search-button" onClick={searchShows}>
+          <button className="search-button" onClick={ searchShows && searchPeople}>
             Search
           </button>
         </div>
@@ -157,7 +197,31 @@ function App() {
                 Add to Favorites
               </button>
             </div>
-          ))} 
+          )
+          )
+          
+          || people.map((person) => (
+            <div key={person.id} className="card">
+              <img src={person.image.medium} alt={person.name} />
+              <h3 className="card-title">{person.name}</h3>
+              <p className="card-country">
+                Country: {person.country}
+              </p>
+              <p className="card-birthday">
+                Birthday: {person.birthday}
+              </p>
+              <p className="card-deathday">
+                Deathday: {person.deathday}
+              </p>
+              {/* <button className="card-button" onClick={() => addFavorite(person)}>
+                Add to Favorites
+              </button> */}
+            </div>
+          ))
+
+        }
+
+
         </div>
 
         <div className="favorites-section">
@@ -167,7 +231,8 @@ function App() {
               <li key={favorite.id} className="favorite">
                 <span
                   onClick={() =>
-                    openModel(shows.find((show) => show.id === favorite.id)!)
+                    openModel(shows.find((show) => show.id === favorite.id)! || openModel2(people.find((person) => person.id === favorite.id)!)
+                    )
                   }
                   className="favorite-name"
                 >
@@ -201,7 +266,27 @@ function App() {
               <p className="modal-description">{selectedShow.summary}</p>
             </div>
           </div>
-        )}
+        ) || selectedPeople && showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModel}>
+                &times;
+              </span>
+              <img src={selectedPeople.image.original} alt={selectedPeople.name} />
+              <h3 className="modal-title">{selectedPeople.name}</h3>
+              <p className="modal-country">
+                Country: {selectedPeople.country}
+              </p>
+              <p className="modal-birthday">
+                Birthday: {selectedPeople.birthday}
+              </p>
+              <p className="modal-deathday">
+                Deathday: {selectedPeople.deathday}
+              </p>
+            </div>
+          </div>
+            ) || null
+        }        
       </div>
 
       <footer>
